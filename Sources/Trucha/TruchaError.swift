678cdb5a6acfd99ba4523df9f414aadff3e7e370
Juan David Hurtado <juandahurtadom@gmail.com>
2023-07-20 12:19:55 -0500

    Add request basics (#2)

---
+++ Sources/Trucha/TruchaError.swift
@@ -0,0 +1,12 @@
+//
+//  TruchaError.swift
+//  
+//
+//  Created by Juan Hurtado on 18/07/23.
+//
+
+import Foundation
+
+enum TruchaError: Error {
+    case invalidURL
+}
--- Tests/TruchaTests/TruchaTests.swift
+++ Tests/TruchaTests/TruchaTests.swift
@@ -5,16 +5,45 @@ final class TruchaTests: XCTestCase {
     let basePath = "https://foo.bar"
     
     override func setUp() {
-        Trucha.sharedClient.setBasePath(basePath)
+        Trucha.sharedClient.clear()
     }
     
-    func test_sharedClientBasePath_shouldBeEqualToTheOneProvided() throws {
+    func test_sharedClientBasePath_shouldBeEqualToTheOneProvided() {
+        Trucha.sharedClient.setBasePath(basePath)
         XCTAssertEqual(Trucha.sharedClient.basePath, basePath)
     }
     
-    func test_requestURL_shouldBeEqualToTheSumOfBasePathAndItsPath() throws {
+    func test_requestUrlshouldBeEqualToTheSumOfBasePathAndItsPath() {
+        Trucha.sharedClient.setBasePath(basePath)
         let path = "/trucha"
         let request = Trucha.sharedClient.request(path)
-        XCTAssertEqual(request.url.absoluteString, basePath + path)
+        XCTAssertNotNil(request)
+        XCTAssertEqual(request!.url.absoluteString, basePath + path)
+    }
+    
+    func test_requestInitWithInvalidPath_shouldThrow() {
+        let invalidPath = "this_is_an_invalid_path"
+        do {
+            let _ = try TruchaRequest(path: invalidPath)
+        } catch {
+            guard let truchaError = error as? TruchaError else {
+                XCTFail("wrong error thrown!!")
+                return
+            }
+            XCTAssertEqual(truchaError, .invalidURL)
+        }
+    }
+    
+    func test_requestInitViaClientWithInvalidPathAndBasePath_shouldBeNil() {
+        Trucha.sharedClient.setBasePath(basePath)
+        let invalidPath = "th1s_is an_invalid_p4th"
+        let request = Trucha.sharedClient.request(invalidPath)
+        XCTAssertNil(request)
+    }
+    
+    func test_requestInitViaClientWithUrlWithoutBasePath_shouldInitSuccessfuly() {
+        let rawUrl = "https://bar.co"
+        let request = Trucha.sharedClient.request(rawUrl)
+        XCTAssertNotNil(request)
     }
 }
