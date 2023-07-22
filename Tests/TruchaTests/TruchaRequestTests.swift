71e65ea7a8646f9ca7035bec9e4b96d399ca2a3f
Juan David Hurtado <juandahurtadom@gmail.com>
2023-07-22 11:53:22 -0500

    Move requests test to a separate file

---
+++ Tests/TruchaTests/TruchaRequestTests.swift
@@ -0,0 +1,54 @@
+//
+//  TruchaRequestTests.swift
+//  
+//
+//  Created by Juan Hurtado on 22/07/23.
+//
+
+import XCTest
+@testable import Trucha
+
+final class TruchaRequestTests: XCTestCase {
+    let basePath = "https://foo.bar"
+    
+    func testRequestUrl_withBasePathSet_shouldBeEqualToTheSumOfBasePathAndItsPath() {
+        Trucha.sharedClient.setBasePath(basePath)
+        let path = "/trucha"
+        let request = Trucha.sharedClient.request(path)
+        XCTAssertNotNil(request)
+        XCTAssertEqual(request!.url.absoluteString, basePath + path)
+    }
+    
+    func testRequestInit_WithInvalidPath_shouldThrow() {
+        let invalidPath = "this_is_an_invalid_path"
+        do {
+            let _ = try TruchaRequest(path: invalidPath)
+        } catch {
+            guard let truchaError = error as? TruchaError else {
+                XCTFail("wrong error thrown!!")
+                return
+            }
+            XCTAssertEqual(truchaError, .invalidURL)
+        }
+    }
+    
+    func testRequestInitViaClient_withInvalidPathAndBasePath_shouldBeNil() {
+        Trucha.sharedClient.setBasePath(basePath)
+        let invalidPath = "th1s_is an_invalid_p4th"
+        let request = Trucha.sharedClient.request(invalidPath)
+        XCTAssertNil(request)
+    }
+    
+    func testRequestInitViaClient_withUrlWithoutBasePath_shouldInitSuccessfuly() {
+        let rawUrl = "https://bar.co"
+        let request = Trucha.sharedClient.request(rawUrl)
+        XCTAssertNotNil(request)
+    }
+    
+    func testRequestStart_withValidUrl_shouldReturnResponse() async throws {
+        let url = "https://api.publicapis.org/entries"
+        let request = Trucha.sharedClient.request(url)
+        let response = try? await request?.start()
+        XCTAssertNotNil(response)
+    }
+}
--- Tests/TruchaTests/TruchaTests.swift
+++ Tests/TruchaTests/TruchaTests.swift
@@ -4,53 +4,9 @@ import XCTest
 final class TruchaTests: XCTestCase {
     let basePath = "https://foo.bar"
     
-    override func setUp() {
-        Trucha.sharedClient.clear()
-    }
-    
-    func test_sharedClientBasePath_shouldBeEqualToTheOneProvided() {
+
+    func testSharedClientBasePath_withProvidedBasePath_shouldBeEqualToTheOneProvided() {
         Trucha.sharedClient.setBasePath(basePath)
         XCTAssertEqual(Trucha.sharedClient.basePath, basePath)
-    }
-    
-    func test_requestUrlshouldBeEqualToTheSumOfBasePathAndItsPath() {
-        Trucha.sharedClient.setBasePath(basePath)
-        let path = "/trucha"
-        let request = Trucha.sharedClient.request(path)
-        XCTAssertNotNil(request)
-        XCTAssertEqual(request!.url.absoluteString, basePath + path)
-    }
-    
-    func test_requestInitWithInvalidPath_shouldThrow() {
-        let invalidPath = "this_is_an_invalid_path"
-        do {
-            let _ = try TruchaRequest(path: invalidPath)
-        } catch {
-            guard let truchaError = error as? TruchaError else {
-                XCTFail("wrong error thrown!!")
-                return
-            }
-            XCTAssertEqual(truchaError, .invalidURL)
-        }
-    }
-    
-    func test_requestInitViaClientWithInvalidPathAndBasePath_shouldBeNil() {
-        Trucha.sharedClient.setBasePath(basePath)
-        let invalidPath = "th1s_is an_invalid_p4th"
-        let request = Trucha.sharedClient.request(invalidPath)
-        XCTAssertNil(request)
-    }
-    
-    func test_requestInitViaClientWithUrlWithoutBasePath_shouldInitSuccessfuly() {
-        let rawUrl = "https://bar.co"
-        let request = Trucha.sharedClient.request(rawUrl)
-        XCTAssertNotNil(request)
-    }
-    
-    func testRequestStart_withValidUrl_shouldReturnResponse() async throws {
-        let url = "https://api.publicapis.org/entries"
-        let request = Trucha.sharedClient.request(url)
-        let response = try? await request?.start()
-        XCTAssertNotNil(response)
     }
 }
